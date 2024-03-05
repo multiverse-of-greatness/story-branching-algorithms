17f3f91ba3562a0a0fc02652b228ffb82900dcbd
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2024-03-05 21:23:27 +0900

    fix: incorrect indentation

--- src/llms/llm.py
+++ src/llms/llm.py
@@ -23,26 +23,26 @@ class LLM(ABC):
         for message in history:
             count_tokens += self.count_token(message["content"])
 
-        if count_tokens > self.max_tokens * 0.8:  # Total tokens is over 80% of the limit
-            count_tokens = 0
-            for message in history[-1:3:-1]:
-                count_tokens += self.count_token(message["content"])
-                if count_tokens > self.max_tokens * 0.5:  # Rolling history until 50% of the limit
-                    break
-                new_history.append(message)
+            if count_tokens > self.max_tokens * 0.8:  # Total tokens is over 80% of the limit
+                count_tokens = 0
+                for message in history[-1:3:-1]:
+                    count_tokens += self.count_token(message["content"])
+                    if count_tokens > self.max_tokens * 0.5:  # Rolling history until 50% of the limit
+                        break
+                    new_history.append(message)
 
-            for message in history[:4][::-1]:
-                new_history.append(message)
+                for message in history[:4][::-1]:
+                    new_history.append(message)
 
-            new_history.reverse()
+                new_history.reverse()
 
-            logger.debug(f"History is over token limit: {count_tokens}/{self.max_tokens}. Rolling history.")
-            logger.debug(
-                f"New token count: {count_tokens}. New history length: {len(new_history)}. New history: {new_history}")
-            return new_history
-        else:
-            logger.debug(f"History is within token limit: {count_tokens}/{self.max_tokens}. No need to roll.")
-            return history
+                logger.debug(f"History is over token limit: {count_tokens}/{self.max_tokens}. Rolling history.")
+                logger.debug(
+                    f"New token count: {count_tokens}. New history length: {len(new_history)}. New history: {new_history}")
+                return new_history
+            else:
+                logger.debug(f"History is within token limit: {count_tokens}/{self.max_tokens}. No need to roll.")
+                return history
 
     @abstractmethod
     def generate_content(self, ctx: 'GenerationContext', messages: ConversationHistory) -> tuple[str, dict]:
