1375b2653338aaabc1bec48ee6566fcd74d7d993
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2024-03-07 14:10:49 +0900

    fix: logic error in calculating token limit

--- src/llms/llm.py
+++ src/llms/llm.py
@@ -19,21 +19,26 @@ class LLM(ABC):
     def rolling_history(self, history: ConversationHistory) -> ConversationHistory:
         logger.debug(f"Starting rolling history with max tokens: {self.max_tokens}")
         count_tokens = 0
-        for message in history:
+        first_part_history_tokens = 0
+        for i, message in enumerate(history):
+            if i < 4:
+                first_part_history_tokens += self.count_token(message["content"])
             count_tokens += self.count_token(message["content"])
 
         if count_tokens > self.max_tokens * 0.8:  # Total tokens is over 80% of the limit
             n_history = len(history)
 
+            start_idx = n_history - 1
             new_history = []
             if history[-1]["role"] == "user":  # If the last message is user message, keep it
                 new_history.append(history[-1])
                 count_tokens = self.count_token(history[-1]["content"])
                 start_idx = n_history - 2  # Start from the latest assistant message
-            elif history[-1]["role"] == "assistant":  # If the last message is assistant message
-                count_tokens = 0
-                start_idx = n_history - 1
+            elif history[-1]["role"] == "assistant":  # If the last message is assistant message, throw an error.
+                # Since this is a history used to interact with LLMs, the last message must be a user message.
+                raise ValueError(f"History is not in the correct conversation format: {history[-1]['role']}")
 
+            # Do it in reverse order
             for idx in range(start_idx, 3, -2):
                 if history[idx]["role"] != "assistant" or history[idx - 1]["role"] != "user":
                     raise ValueError(f"History is not in the correct conversation format: "
@@ -43,11 +48,12 @@ class LLM(ABC):
                 user_message = history[idx - 1]
                 count_tokens += self.count_token(assistant_message["content"]) + self.count_token(
                     user_message["content"])
-                if count_tokens > self.max_tokens * 0.5:  # Rolling history until 50% of the limit
+                if count_tokens > self.max_tokens * 0.6 - first_part_history_tokens:  # Rolling history until 60%
                     break
                 new_history.append(assistant_message)
                 new_history.append(user_message)
 
+            # Keep story data and the first story chunk
             for message in history[:4][::-1]:
                 new_history.append(message)
 
