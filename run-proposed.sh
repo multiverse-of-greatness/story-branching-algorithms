2595a68eed1c93e3fefdf25de67d205352239213
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2024-03-07 16:12:14 +0900

    chore: change default choice of running generation

--- run-proposed.sh
+++ run-proposed.sh
@@ -1,10 +1,9 @@
 #!/bin/zsh
 python generate.py \
   --approach proposed \
-  --num-chapters 1 \
-  --min-num-choices 3 \
+  --num-chapters 3 \
+  --min-num-choices 2 \
   --max-num-choices 3 \
-  --min-num-choices-opportunity 2 \
+  --min-num-choices-opportunity 1 \
   --max-num-choices-opportunity 2 \
-  --enable-image-generation \
-  --existing-plot outputs/proposed/1aa8a898-dbcb-11ee-a544-baf3908e4dbe/plot.json
+  --enable-image-generation
