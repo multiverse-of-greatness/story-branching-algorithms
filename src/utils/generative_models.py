f6168a5969c1af49bf177c9b6c8bc015b428a3d2
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2024-03-07 11:36:02 +0900

    feat: add max_tokens for each model

--- src/utils/generative_models.py
+++ src/utils/generative_models.py
@@ -6,14 +6,26 @@ from src.llms.google_model import GoogleModel
 from src.llms.llm import LLM
 from src.llms.openai_model import OpenAIModel
 
+MAX_TOKENS = {
+    'gpt-3.5-turbo-0125': 16385,
+    'gpt-4-turbo-preview': 128000,
+    'gemini-1.0-pro': 32768,
+    'claude-3-opus-20240229': 200000,
+    'claude-3-sonnet-20240229': 200000,
+    'claude-2.1': 200000,
+}
+
 
 def get_generation_model(model_name: str) -> LLM:
     if model_name in ["gpt-3.5-turbo-0125", "gpt-4-turbo-preview"]:
-        return OpenAIModel(model_name)
+        max_tokens = MAX_TOKENS[model_name]
+        return OpenAIModel(model_name, max_tokens)
     elif model_name in ["gemini-1.0-pro"]:
-        return GoogleModel(model_name)
+        max_tokens = MAX_TOKENS[model_name]
+        return GoogleModel(model_name, max_tokens)
     elif model_name in ["claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-2.1"]:
-        return AnthropicModel(model_name)
+        max_tokens = MAX_TOKENS[model_name]
+        return AnthropicModel(model_name, max_tokens)
     else:
         raise ValueError(f"Unknown generation model: {model_name}")
 
